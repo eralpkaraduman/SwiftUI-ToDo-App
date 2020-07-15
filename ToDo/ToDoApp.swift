81cdf55dce5ee4c5752f47b5ba2d331f1eb963f8
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-07-15 22:46:04 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Eralp Karaduman on 15.7.2020.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
